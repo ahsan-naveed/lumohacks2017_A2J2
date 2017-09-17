9d7e3904a31252e763ac4102047543e74b24e9d7
Ahsan Naveed <ahsannaveed007@yahoo.com>
2017-09-17 01:14:33 -0700

    Update chatebot.js

--- chatbot.js
+++ chatbot.js
@@ -19,14 +19,60 @@ var bot = new builder.UniversalBot(connector);
 // connector.listen() to catch messages on the /api/messages route
 server.post('/api/messages', connector.listen());
 
+let userMeds = []; // will store user selected meds from medsCategories
+let usersDepCause; // stores user depression cause
+let usersDoctorInfo = {}; // stores user's doctor information
+let usersPharmacyInfo = {}; // sotres user's pharmacy information
+
+// categories of medications - can be expanded
+var medCategories = {
+    "SSRIs": {
+        meds: [
+        	   'escitalopram', 
+        	   'citalopram', 
+        	   'fluoxetine', 
+        	   'fluvoxamine', 
+        	   'paroxetine', 
+        	   'sertraline'
+        	  ] 
+    },
+    "TCAs": {
+        meds: [
+        	   'nortriptyline',
+        	   'amitriptyline',
+        	   'doxepin'
+        	  ]
+    },
+    "SNRIs": {
+        meds: [
+        	   'venlafaxine',
+        	   'desvenlafaxine',
+        	   'duloxetine'
+        	  ]
+    },
+    "serotoninModulators": {
+    	meds: [
+    			'trazodone'
+    		  ]
+    }
+}; 
+
+// causes of depression
+let depCauses = ['Sleep?',
+				'Drugs?',
+				'Smoking?',
+				'Anxiety?',
+				'Apetite?'
+	]
+
 /* 
-BOT DIALOG IMPLEMENTATION
+** BOT DIALOG IMPLEMENTATION - WATERFALL
 */
 
 // demographics
 bot.dialog('/', function (session, args) {
     if (!session.userData.greeting) {
-        session.send("Hi! I’d like to ask you some questions about your general health!. What is your name?");
+        session.send("Hi! I’d like to ask you some questions about your general health!. Let's start with your name?");
         session.userData.greeting = true;
     } else if (!session.userData.name) {
         getName(session);
@@ -36,11 +82,18 @@ bot.dialog('/', function (session, args) {
         getGender(session);
     } else if(!session.userData.age){
     	getAge(session);
-    } else {
+    } else if (!session.userData.medications){
+    	getMedications(session);
+	} else if (!session.userData.therapyDate) {
+		getTherapyDate(session);
+	} else if(!session.userData.depressionCause){
+		getDepCause(session);
+	} else {
         session.userData = null;
     }
     session.endDialog();
 });
+
 
 function getName(session) {
 	name = session.message.text;
@@ -55,17 +108,85 @@ function getEmail(session) {
         session.userData.email = email;
         session.send("Thank you, " + session.userData.name );
     } 
-    session.send("What is your gender? Type M for male or F for female.")
+    session.send("What is your gender? Type M for male, F for female, T for trans or other.")
 }
 
 function getGender(session) {
 	gender = session.message.text;
 	session.userData.gender = gender;
-	session.send("What is your age?")
+	if (gender.toUpperCase() === "M") {
+		session.send("How old are you, Sir?")
+	} else {
+		session.send("How old are you, Mam?")
+	}
 }
 
 function getAge(session) {
 	age = session.message.text;
 	session.userData.age = age;
-	session.send("Thanks");
+	builder.Prompts.choice(session, "Please select the medication category you are starting on - type in the respective number: ", medCategories);
+}
+
+function getMedications(session) {
+	medications = session.message.text;
+	session.userData.medications = medications;
+	console.log(medications);
+	switch(medications){
+		case '1':
+			session.send("You selected SSRIs which inlcude " + medCategories.SSRIs.meds.toString());
+			userMeds.push(medCategories.SSRIs.meds);
+			break;
+		case '2':
+			session.send("You selected TCAs which inlcude " + medCategories.TACs.meds.toString());
+			userMeds.push(medCategories.TCAs.meds);
+			break;
+		case '3':
+			session.send("You selected SNRIs which inlcude " + medCategories.SNRIs.meds.toString());
+			userMeds.push(medCategories.SNRIs.meds);
+			break;
+		case '4':
+			session.send("You selected Serotonin Modulators which inlcude " + medCategories.serotoninModulators.meds.toString());
+			userMeds.push(medCategories.serotoninModulators.meds);
+			break;
+		default:
+			// user selected other
+			session.send("You didn't select any category."); 
+			break;
+	}
+	console.log(userMeds);
+	session.send("When do you start your therapy?");
+}
+
+function getTherapyDate(session) {
+	therapyDate = session.message.text;
+	session.userData.therapyDate = therapyDate;
+	builder.Prompts.choice(session, "Do you worry about any of the following - type in the respective number: ", depCauses);
+}
+ 
+function getDepCause(session) {
+	depressionCause = session.message.text;
+	session.userData.depressionCause = depressionCause;
+	switch(depressionCause){
+		case '1':
+			session.send("You selected 'Sleep' as your depression cause.");
+			usersDepCause = 'sleep';
+			break;
+		case '2':
+			session.send("You selected 'Drugs' as your depression cause.");
+			usersDepCause = 'drugs';
+			break;
+		case '3':
+			session.send("You selected 'Smoking' as your depression cause.");
+			usersDepCause = 'smoking';
+			break;
+		case '4':
+			session.send("You selected 'Apetite' as your depression cause.");
+			usersDepCause = 'apetite';
+			break;
+		default:
+			// no option selected 
+			session.send("I'm glad you don't have any depression cause.");
+			break;
+	}
+	session.send("Thanks for providing all the information. Now I would like to collect some information about your doctor - please provide their name, address and phone number: ");
 }
