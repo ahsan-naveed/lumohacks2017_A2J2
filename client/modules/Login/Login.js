a1973556ab3400b236317da752b74138db452658
John L. Cao <john_caohaha@hotmail.com>
2017-09-16 20:03:08 -0700

    required contraint for username and password

--- client/modules/Login/Login.js
+++ client/modules/Login/Login.js
@@ -5,16 +5,37 @@ class Login extends React.Component {
   constructor(props){
     super(props);
     this.state={
-    username:'',
-    password:''
+      username:'',
+      password:''
     }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.submitIfValid = this.submitIfValid.bind(this);
+
    }
+
+   handleUsernameChange(e){
+     this.setState({username: e.target.value});
+   }
+
+   handlePasswordChange(e){
+     this.setState({password: e.target.value});
+   }
+
+   submitIfValid(){
+     if(this.state.username.length && this.state.password.length){
+       redirect('surveys');
+     }
+   }
+   
   render() {
       return (
         <div>
-            <input type="text" placeholder="Enter Username" name="uname" required/> <br/>
-            <input type="password" placeholder="Enter Password" name="psw" required/> <br/>
-            <button type="submit" onClick={()=>redirect('surveys')}>Log Me In!</button>
+          <form>
+            <input type="text" placeholder="Enter Username" onChange={this.handleUsernameChange}/> <br/>
+            <input type="password" placeholder="Enter Password" onChange={this.handlePasswordChange}/> <br/>
+            <button onClick={this.submitIfValid}>Log Me In!</button>
+          </form>
         </div>
       );
   }
