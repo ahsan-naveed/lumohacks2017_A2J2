6ee397f4b9a51694fdaeeb25b1c1cdc194b59a7f
John Lee <PiggySpeed@users.noreply.github.com>
2017-09-17 03:44:35 -0700

    minor navbar changes

--- client/modules/MainContainer/NavBar.js
+++ client/modules/MainContainer/NavBar.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { redirect } from '../../routes.js';
 
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    position: 'absolute',
+    backgroundColor: 'white',
+    opacity: 0.8,
+    zIndex: 99,
+  },
+};
+
 const NavBar = () => (
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
+  <div style={styles.container}>
     <button onClick={() => redirect('/')}>Login</button>
     <button onClick={() => redirect('surveys')}>Surveys</button>
     <button onClick={() => redirect('chatbot')}>ChatBot</button>
