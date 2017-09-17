af1e07dcba42b53c5f7147297352eaef517c3a26
John L. Cao <john_caohaha@hotmail.com>
2017-09-17 11:27:13 -0700

    new bot workd

--- client/modules/ChatBot/ChatBot.js
+++ client/modules/ChatBot/ChatBot.js
@@ -5,14 +5,16 @@ import styles from './ChatBot.css';
 export default class ChatBot extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      bot_url: 'https://webchat.botframework.com/embed/maxbi?t=EvK7YdJ3dZo.dAA.NQBvAHAAWgBuADEAYwBVADMAYwB5AEUATwA3AFAAaQAwADgAcgB4AEYAbAA.IkPkcuUv0wE.PKlBW4ncVUk.cLN-Eun03nOg-CkpKhTE-rCyRL1QqzcWymqCC6jNCrE'
+    };
   }
 
   render() {
     return (
       <div className={styles.mainbody}>
         <Header />
-        <iframe src='http://webchat.botframework.com/embed/maxbi?&t=EvK7YdJ3dZo.dAA.NgBPAGkAbgBhADgASQBOAEsAcQBqADYAagB1AG0AdgBOAGoAVABEAFkAUgA.8K6RIt8v0wE.tGDNpzQINRQ.EPODG_8qO9EshT9XAmRB_TzNb3omqHR4T89K1jF7m_c'></iframe>
+        <iframe src={this.state.bot_url}></iframe>
       </div>
     );
   }
